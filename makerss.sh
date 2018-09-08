b6bc08ef4f992ef5887100338827ea730ee1d3fa
hellokh4312 <33303972+hellokh4312@users.noreply.github.com>
2018-09-08 15:17:33 +0900

    Delete makerss.sh
    del

--- makerss.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-PATH_MAKERSS=/volume1/video/makerss
-PATH_GIT=/volume1/video/git/hellokh4312.github.io
-
-cd $PATH_MAKERSS
-python makerss_main.py
-mv $PATH_MAKERSS/*.xml $PATH_GIT
-$PATH_GIT/commit.sh
-
