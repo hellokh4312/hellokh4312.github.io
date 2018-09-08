1d7c33f8585cbbc022cf5b06e776d160c98bca99
hellokh4312 <yongchunwoo@naver.com>
2018-09-08 14:40:13 +0900

    20180908-1440

--- makerss.sh
+++ makerss.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-PATH_MAKERSS=/volume1/
-PATH_GIT=/volume1/
+PATH_MAKERSS=/volume1/video/makerss
+PATH_GIT=/volume1/video/git/hellokh4312.github.io
 
 cd $PATH_MAKERSS
 python makerss_main.py
